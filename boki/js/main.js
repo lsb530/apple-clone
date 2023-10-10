128289d2cad0da90e272352497be7e7947d001bd
seung.bok <seungbok.lee@huinnoaim.com>
2023-10-10 15:24:44 +0900

    마지막 문단 애니메이션

--- boki/js/main.js
+++ boki/js/main.js
@@ -134,6 +134,8 @@
         rect2X: [0, 0, { start: 0, end: 0 }],
         blendHeight: [0, 0, { start: 0, end: 0 }],
         canvas_scale: [0, 0, { start: 0, end: 0 }],
+        canvasCaption_opacity: [0, 1, { start: 0, end: 0}],
+        canvasCaption_translateY: [20, 0, { start: 0, end: 0}],
         rectStartY: 0
       },
     },
@@ -474,6 +476,15 @@
             && values.canvas_scale[2].end > 0) {
             objs.canvas.classList.remove('sticky');
             objs.canvas.style.marginTop = `${scrollHeight * 0.4}px`;
+
+            values.canvasCaption_opacity[2].start = values.canvas_scale[2].end;
+            values.canvasCaption_opacity[2].end = values.canvasCaption_opacity[2].start + 0.1;
+            objs.canvasCaption.style.opacity = calcValues(values.canvasCaption_opacity, currentYOffset);
+
+            values.canvasCaption_translateY[2].start = values.canvas_scale[2].end;
+            values.canvasCaption_translateY[2].end = values.canvasCaption_opacity[2].start + 0.1;
+            objs.canvasCaption.style.transform =
+              `translate3d(0, ${calcValues(values.canvasCaption_translateY, currentYOffset)}%, 0)`;
           }
         }
 
